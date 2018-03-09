8540852fce7b124e38aa23fbfc5931242cb3b99d
Anthony Liu <tlrnalra@gmail.com>
2018-03-09 03:51:56 -0500

    add permissions for judges to see their dashboard

--- src/client/components/judging/Judge.tsx
+++ src/client/components/judging/Judge.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import Header from '../Header';
+import Navigation from '../Navigation';
+import { Switch, Route } from 'react-router';
+import Judging from './Judging';
+import Expo from '../expo/Expo';
+import Status from '../Status';
 // import './Judge.scss';
 
 interface JudgeProps {}
@@ -6,8 +12,15 @@ interface JudgeProps {}
 const Judge: React.SFC<JudgeProps> = (props) => {
     return (
         <div>
-            <h1>Welcome!</h1>
-            <p>You have been approved as a Judge!</p>
+            <Header />
+            <Navigation
+                linkNames={['Home', 'Judging', 'Expo']}
+                linkDests={['/', '/judging', '/expo']} />
+            <Switch>
+                <Route exact path='/judging' component={Judging} />
+                <Route exact path='/expo' component={Expo} />
+                <Route exact path='/' component={Status} />
+            </Switch>
         </div>
     );
 };
