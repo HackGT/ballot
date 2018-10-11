ad95aca280a9eac58cbac049072ec26d44200932
Ehsan Asdar <ehsanmasdar@gmail.com>
2018-10-11 18:57:57 -0400

    Changes for deployment

--- server/scripts/postinstall.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// .env file
-if(fs.existsSync('.env')){
-    console.log('Found .env file, moving forward')
-} else {
-    fs.createReadStream('.env.example').pipe(fs.createWriteStream('.env'));
-    console.log('.env file created');
-}
