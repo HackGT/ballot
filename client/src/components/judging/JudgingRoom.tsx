b11a520630f8949cbe354b6c7f6937618e3f3164
Elton <eltonp3103@gmail.com>
2019-03-19 17:31:28 -0400

    initial changes

---
+++ client/src/components/judging/JudgingRoom.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface JudgingRoomProps {
+    table: string;
+}
+
+class JudgingRoom extends React.Component<JudgingRoomProps, {}>{
+    public render() {
+        return <h1>This is a test</h1>
+    }
+
+}
+
+export default JudgingRoom;
