140c78022f90a5eb61c649dc14ec0cb6b026be2a
Michael Raman <michael.raman@me.com>
2021-09-25 15:54:32 -0400

    Adding build folder to resolve last minute deployment issue

---
+++ client/build/precache-manifest.a528dea505573c9dc803ef7b17d0db34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54df82ed66fc0d0370dfbf0c84d124d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2c449f4829735ca082",
+    "url": "/static/css/main.498dfaa0.chunk.css"
+  },
+  {
+    "revision": "ef1c6fbde192e3cacf3b",
+    "url": "/static/js/2.e657e8ae.chunk.js"
+  },
+  {
+    "revision": "1e2c449f4829735ca082",
+    "url": "/static/js/main.6259ef67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
